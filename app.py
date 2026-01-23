ac46dceafb813e0804d5bd08a420ec23a364db5e
Guilherme Santos <106110470+guileals@users.noreply.github.com>
2026-01-23 14:17:30 +0000

    Aula 23/01 - Criação de rotas

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from datetime import datetime
 from config.database import SupabaseConnection
 from dao.funcionario_dao import FuncionarioDAO
 
@@ -9,6 +10,14 @@ client = SupabaseConnection().client
 # Criando DAO para acessar a tabela funcionario
 funcionario_dao = FuncionarioDAO(client)
 
+# Filtro personalizado para formatar CPF
+@app.template_filter('format_cpf')
+def format_cpf(cpf):
+    """Formata CPF no padrão XXX.XXX.XXX-XX"""
+    if not cpf or len(cpf) != 11:
+        return cpf
+    return f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:11]}"
+
 @app.route("/")
 def index():
     return render_template("index.html", title="3INF1M", app_name="Meu Flask App", funcionarios=funcionario_dao.read_all())
@@ -16,4 +25,4 @@ def index():
 @app.route("/funcionario/<string:pk>/<int:id>")
 def details(pk, id):
     funcionario = funcionario_dao.read(pk, id)
-    return render_template("details.html", funcionario=funcionario)
+    return render_template("details.html", funcionario=funcionario, datetime=datetime)
